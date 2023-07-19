4194e21e56b7602c016f53bfc91eadadaed27f59
Amrutha <amrutha@zelarsoft.com>
2023-07-19 16:07:51 +0530

    Update EmployeeService.js

--- frontend-react/src/services/EmployeeService.js
+++ frontend-react/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_API_URL; //backend service name
+const url = process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1/employees" ; //backend service name
 
 class EmployeeService {
 
